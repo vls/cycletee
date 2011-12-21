5dce03b6039ec88532a31b2ba83f58338e1cd911
ukessi <liangvls@gmail.com>
2011-12-21 16:03:04 +0800

    add benchmark

---
+++ benchmark.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import time
+import os
+
+def run():
+    t0 = time.time()
+
+    os.system('seq 300000 | bin/cycletee > /dev/null')
+
+    total = time.time() - t0
+    total *= 1000
+    print "Cost time = %f" % total 
+    return total
+
+
+
+def main():
+    arr = []
+    for i in xrange(10):
+        arr.append(run())
+
+    print "Avg time = %f" % (sum(arr) / len(arr))
+
+
+
+
+if __name__ == '__main__':
+    main()
+
